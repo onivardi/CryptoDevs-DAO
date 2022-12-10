23875c42586924ebe896902e220e33c12d9bbc38
Valdeir <vardii.rodrigues@gmail.com>
2022-12-10 12:45:19 -0400

    condition renderer for the button

--- next-dapp/src/components/Whitelist/WhitelistContent.tsx
+++ next-dapp/src/components/Whitelist/WhitelistContent.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import { PrepareWriteContractResult } from '@wagmi/core';
+import React, { MouseEventHandler } from 'react';
+
 
 type WhitelistProps = {
     numJoined: string | undefined;
-    handleJoin: any ;
+    handleJoin:  any;
+    isConnected: boolean;
+    isWhitelisted: boolean | undefined
 }
 
-export function WhitelistContent({ numJoined, handleJoin }: WhitelistProps) {
+export function WhitelistContent({ numJoined, handleJoin, isConnected, isWhitelisted }: WhitelistProps) {
     
+    function TextRender() {
+        if (!isConnected) {
+            return 'Connect your Wallet'
+        } else if (isWhitelisted) {
+            return 'Your are In!'
+        } else if(isConnected){
+            return 'Join the Whitelist'
+        }
+    }
+
+
     return (
-        <div className="box">
-            <div className="content has-text-centered">
+        <div className="box py-6">
+            <div className="content has-text-centered my-6">
                 <p className='title is-4'>Joined to the Whitelist</p>
                 <p className='is-size-3'>{numJoined}/10</p>
-                <button className="button is-primary" disabled={!handleJoin} onClick={handleJoin}>Join the Whitelist</button>
+                <button className="button is-primary" onClick={() =>handleJoin} disabled={!handleJoin || isWhitelisted}> {TextRender()}</button>
+
             </div>
         </div>
     );
