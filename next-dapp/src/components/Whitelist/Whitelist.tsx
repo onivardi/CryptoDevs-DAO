0ecc8c930b810447210b17e1d8cd471b287fce9d
Valdeir <vardii.rodrigues@gmail.com>
2022-12-09 20:16:51 -0400

    add usecontractread and usecontractwrite

--- next-dapp/src/components/Whitelist/Whitelist.tsx
+++ next-dapp/src/components/Whitelist/Whitelist.tsx
@@ -1,18 +1,44 @@
 import React from 'react'
+import { useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { whitelistABI, whitelistADDRESS } from '../../../CONSTANTS'
 import { WhitelistContent } from './WhitelistContent'
 
-type Props = {}
 
-export const Whitelist = (props: Props) => {
+
+export const Whitelist = () => {
+
+  const whitelistContract = {
+    address: whitelistADDRESS,
+    abi: whitelistABI
+  }
+
+
+  const { data: numAddrWhitelisted } = useContractRead({
+    ...whitelistContract,
+    functionName: 'numAddressesWhitelisted',
+    onSettled(numAddrWhitelisted: string | undefined, error) {
+
+    },
+    watch: true
+  })
+
+  const { config } = usePrepareContractWrite({
+    ...whitelistContract,
+    functionName: 'addAddressToWhitelist'
+  })
+
+  const { data, isSuccess, write, writeAsync } = useContractWrite(config)
+  {console.log(write)}
   return (
     <section className="section">
-        <div className="container">
-            <div className="columns is-centered">
-                <div className="column is-5-tablet is-4-desktop">
-                    <WhitelistContent />
-                </div>
-            </div>
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-5-tablet is-4-desktop">
+            <WhitelistContent numJoined={numAddrWhitelisted} handleJoin={writeAsync}/>
+            {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+          </div>
         </div>
+      </div>
     </section>
   )
 }
--- next-dapp/src/components/Whitelist/WhitelistContent.tsx
+++ next-dapp/src/components/Whitelist/WhitelistContent.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-export function WhitelistContent() {
+type WhitelistProps = {
+    numJoined: string | undefined;
+    handleJoin: any ;
+}
+
+export function WhitelistContent({ numJoined, handleJoin }: WhitelistProps) {
+    
     return (
         <div className="box">
             <div className="content has-text-centered">
                 <p className='title is-4'>Joined to the Whitelist</p>
-                <p className='is-size-3'>3/10</p>
-                <button className="button is-primary">Join the Whitelist</button>
+                <p className='is-size-3'>{numJoined}/10</p>
+                <button className="button is-primary" disabled={!handleJoin} onClick={handleJoin}>Join the Whitelist</button>
             </div>
         </div>
     );
